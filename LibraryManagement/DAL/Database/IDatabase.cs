9eb66d161bc818fa5793c3ec1124b48ba5d8d05c
im4everok <imizyk09@gmail.com>
2025-10-21 22:08:14 +0200

    added database abstraction / json implementation

---
+++ LibraryManagement/DAL/Database/IDatabase.cs
@@ -0,0 +1,8 @@
+﻿namespace DAL.Database
+{
+    public interface IDatabase<T>
+    {
+        public Task<List<T>> GetAll();
+        public Task<bool> SaveData(List<T> data);
+    }
+}
---
+++ LibraryManagement/DAL/Database/JsonDatabase.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+
+using DAL.Models;
+
+namespace DAL.Database
+{
+    public class JsonDatabase(string fileName) : IDatabase<Book>
+    {
+        private readonly string _fullPath = Path.Combine(AppContext.BaseDirectory, fileName);
+
+        private async Task CreateJsonDbIfNotExists()
+        {
+            if (!File.Exists(_fullPath))
+            {
+                await File.WriteAllTextAsync(_fullPath, "[]");
+            }
+        }
+
+        public async Task<List<Book>> GetAll()
+        {
+            await CreateJsonDbIfNotExists();
+
+            using var stream = File.OpenRead(_fullPath);
+            try
+            {
+
+                List<Book>? books = await JsonSerializer.DeserializeAsync<List<Book>>(stream);
+                return books ?? [];
+            }
+            catch (JsonException)
+            {
+                // If the JSON is invalid, return an empty list
+                return [];
+            }
+        }
+
+        public async Task<bool> SaveData(List<Book> data)
+        {
+            await CreateJsonDbIfNotExists();
+
+            try
+            {
+
+                string serializedData = JsonSerializer.Serialize(data, new JsonSerializerOptions()
+                {
+                    WriteIndented = true
+                });
+
+                await File.WriteAllTextAsync(_fullPath, serializedData);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
