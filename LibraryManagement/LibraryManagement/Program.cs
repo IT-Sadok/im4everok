6a1688cd57e1e1a6aa369248301673e3462d3424
im4everok <imizyk09@gmail.com>
2025-10-25 23:18:19 +0200

    used app strategy - pick either Console UI or load testing

--- LibraryManagement/LibraryManagement/Program.cs
+++ LibraryManagement/LibraryManagement/Program.cs
@@ -14,4 +14,13 @@ IBookValidator addedBookValidator = new AddedBookValidator();
 IBookService bookService = new BookService(bookRepository, addedBookValidator);
 
 var consoleUI = new ConsoleUI(bookService);
-await consoleUI.Run();
+LoadTest loadTest = new(bookService);
+
+var strategies = new List<LibraryManagement.Strategies.AppStrategy>
+{
+    new LibraryManagement.Strategies.AppStrategy(1, "Run Console UI", consoleUI.Run),
+    new LibraryManagement.Strategies.AppStrategy(2, "Run Load Test", loadTest.Run)
+};
+
+var application = new LibraryManagement.Strategies.Application(strategies);
+await application.Run();
