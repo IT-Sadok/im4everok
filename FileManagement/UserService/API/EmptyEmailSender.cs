5c7cfb1f6dd3da446024a7afb03e0d43af19ba8a
im4everok <ihor.mizyk@rib-software.com>
2025-11-15 23:18:41 +0100

    added DB context, identity config, mock email sender, program file

---
+++ FileManagement/UserService/API/EmptyEmailSender.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace FileService.API
+{
+    public class EmptyEmailSender<TUser> : IEmailSender<TUser> where TUser : class
+    {
+        public Task SendConfirmationLinkAsync(TUser user, string email, string confirmationLink)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task SendPasswordResetCodeAsync(TUser user, string email, string resetCode)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task SendPasswordResetLinkAsync(TUser user, string email, string resetLink)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ FileManagement/UserService/API/Program.cs
@@ -0,0 +1,45 @@
+using FileService.API;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DbConnection"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, EmptyEmailSender<ApplicationUser>>();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.MapIdentityApi<ApplicationUser>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
