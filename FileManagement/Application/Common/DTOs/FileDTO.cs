ea1ac284a9671f7fbf35ca5d8856d9097c6b3359
im4everok <ihor.mizyk@rib-software.com>
2025-11-17 23:40:12 +0100

    added file entity / dto

---
+++ FileManagement/Application/Common/DTOs/FileDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Common.DTOs
+{
+    public class FileDTO
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string FileName { get; set; }
+        public float Size { get; set; }
+    }
+}
---
+++ FileManagement/Domain/Entities/File.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class File
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string FileName { get; set; }
+        public float Size { get; set; }
+    }
+}
