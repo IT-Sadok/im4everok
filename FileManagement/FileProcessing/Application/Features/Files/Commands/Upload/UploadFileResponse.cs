0ba8401e616f95ecc238e349f087f99906bbe8e4
im4everok <ihor.mizyk@rib-software.com>
2025-11-25 00:09:21 +0100

    UploadFileService mockup - writing method

---
+++ FileManagement/FileProcessing/Application/Features/Files/Commands/Upload/UploadFileResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Features.Files.Commands.Upload
+{
+    public class UploadFileResponse
+    {
+    }
+}
---
+++ FileManagement/FileProcessing/Application/Features/Files/Commands/Upload/UploadFileService.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Interfaces.Repositories;
+
+using Domain.Entities;
+
+namespace Application.Features.Files.Commands.Upload
+{
+    public class UploadFileService(IFileRepository fileRepository, IUnitOfWork unitOfWork)
+    {
+        public async Task<UploadFileResponse> Execute(UploadFileRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+
+                var file = new FileEntity
+                {
+                    CreatedAt = DateTime.Now,
+                    FileName = request.File.FileName,
+                    Size = request.File.Size,
+                };
+
+                await fileRepository.Add(file);
+                await unitOfWork.SaveChanges();
+
+                return new UploadFileResponse();
+            }
+            catch
+            {
+                return new UploadFileResponse();
+            }
+        }
+    }
+}
