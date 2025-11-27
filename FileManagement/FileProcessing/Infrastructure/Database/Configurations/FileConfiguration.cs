892aaa357d1347e1819025a9154cf3b6f8fdd532
im4everok <ihor.mizyk@rib-software.com>
2025-11-27 23:57:20 +0100

    added file table entity config

---
+++ FileManagement/FileProcessing/Infrastructure/Database/Configurations/FileConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.Configurations
+{
+    internal class FileConfiguration : IEntityTypeConfiguration<FileEntity>
+    {
+        public void Configure(EntityTypeBuilder<FileEntity> builder)
+        {
+            builder.HasKey(f => f.Id);
+        }
+    }
+}
