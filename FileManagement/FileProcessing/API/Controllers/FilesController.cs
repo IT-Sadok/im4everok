debc7dc4b0147f01ba7c1a0f9af1afb69834c3e9
im4everok <ihor.mizyk@rib-software.com>
2025-12-04 23:58:12 +0100

    added upload file endpoint

---
+++ FileManagement/FileProcessing/API/Controllers/FilesController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Files.Commands.Upload;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController(UploadFileService uploadFileService) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadFile([FromForm] IFormFile file)
+        {
+            var cancellationToken = HttpContext.RequestAborted;
+
+            var request = new UploadFileRequest(file.Name, file.OpenReadStream(), file.ContentType);
+            var response = await uploadFileService.Execute(request, cancellationToken);
+
+            return Ok(response);
+        }
+    }
+}
