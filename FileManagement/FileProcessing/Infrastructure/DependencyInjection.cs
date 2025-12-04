4f314430e64b962406e628fcbf1b5c8061c5be0b
im4everok <ihor.mizyk@rib-software.com>
2025-12-04 23:57:57 +0100

    added fileRepo, uow to DI

--- FileManagement/FileProcessing/Infrastructure/DependencyInjection.cs
+++ FileManagement/FileProcessing/Infrastructure/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Infrastructure.Database;
+﻿using Application.Interfaces;
+using Application.Interfaces.Repositories;
+
+using Infrastructure.Database;
+using Infrastructure.Repositories;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -14,6 +18,9 @@ namespace Infrastructure
                 options.UseSqlServer(configuration.GetSection("DbConnection").Value,
                 sql => sql.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)
             ));
+
+            services.AddScoped<IFileRepository, FileRepository>();
+            services.AddScoped<IUnitOfWork, EFUnitOfWork>();
 
             return services;
         }
