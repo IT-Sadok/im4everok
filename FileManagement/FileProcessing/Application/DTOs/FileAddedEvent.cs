986f2765f515c87e7658fbd4648386a3fc33b81e
im4everok <ihor.mizyk@rib-software.com>
2025-12-23 23:58:54 +0100

    added fileAddedEvent, write to DB as outbox when uploading a file

---
+++ FileManagement/FileProcessing/Application/DTOs/FileAddedEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.DTOs
+{
+    public class FileAddedEvent
+    {
+        public Guid FileId { get; set; }
+        public string FileName { get; set; } = default!;
+        public long SizeBytes { get; set; }
+        public string ContentType { get; set; } = default!;
+        public string BlobPath { get; set; } = default!;
+        public string ContainerName { get; set; } = default!;
+        public DateTime CreatedAtUtc { get; set; }
+    }
+}
--- FileManagement/FileProcessing/Application/Features/Files/Commands/Upload/UploadFileService.cs
+++ FileManagement/FileProcessing/Application/Features/Files/Commands/Upload/UploadFileService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Security.Cryptography;
-﻿using Application.Interfaces;
+
+using Application.DTOs;
+using Application.Interfaces;
 using Application.Interfaces.FileStorage;
 using Application.Interfaces.Repositories;
 
@@ -7,8 +9,10 @@ using Domain.Entities;
 
 namespace Application.Features.Files.Commands.Upload
 {
-    public class UploadFileService(IFileRepository fileRepository, IUnitOfWork unitOfWork, IFileStorage fileStorage)
+    public class UploadFileService(IFileRepository fileRepository, IOutboxEventRepository outboxRepository, IUnitOfWork unitOfWork, IFileStorage fileStorage)
     {
+        const string FILE_ADDED_EVENT_NAME = "FileAddedEvent";
+
         string CalculateMD5(Stream file)
         {
             using var md5 = MD5.Create();
@@ -22,19 +26,47 @@ namespace Application.Features.Files.Commands.Upload
             {
                 Guid fileId = Guid.NewGuid();
 
+                // Calculate checksum
                 var checksum = CalculateMD5(request.Content);
+
+                // CRITICAL: Reset stream position after reading for checksum
+                request.Content.Position = 0;
+
                 var file = new FileEntity
                 {
                     Id = fileId,
-                    CreatedAt = DateTime.Now,
+                    CreatedAt = DateTime.UtcNow,
                     FileName = request.FileName,
                     SizeBytes = request.Content.Length,
                     ContentType = request.ContentType,
+                    ContainerName = "files",
                     BlobPath = fileId.ToString(),
                     Checksum = checksum
                 };
 
+                var fileAddedEvent = new FileAddedEvent()
+                {
+                    FileId = file.Id,
+                    FileName = file.FileName,
+                    SizeBytes = file.SizeBytes,
+                    ContentType = file.ContentType,
+                    BlobPath = file.BlobPath,
+                    ContainerName = file.ContainerName,
+                    CreatedAtUtc = file.CreatedAt
+                };
+
+                var outboxEvent = new OutboxEvent()
+                {
+                    Content = System.Text.Json.JsonSerializer.Serialize(fileAddedEvent),
+                    Id = Guid.NewGuid(),
+                    Error = null,
+                    OccuredOnUtc = DateTime.UtcNow,
+                    RetryCount = 0,
+                    Type = FILE_ADDED_EVENT_NAME
+                };
+
                 await fileRepository.Add(file);
+                await outboxRepository.Add(outboxEvent);
                 await unitOfWork.SaveChangesAsync();
 
                 await fileStorage.UploadAsync(fileId.ToString(), request.Content, "files", request.ContentType, ct);
