0584dca058b2f8130b66417c3be209365eccfb6e
im4everok <ihor.mizyk@rib-software.com>
2025-12-23 23:53:29 +0100

    auto apply file processing migrations

--- FileManagement/FileProcessing/API/Program.cs
+++ FileManagement/FileProcessing/API/Program.cs
@@ -11,6 +11,12 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.MapControllers();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
