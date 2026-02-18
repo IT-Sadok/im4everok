e6028d6b615d7e4c0df2f2e7289511852a07ac2e
im4everok <ihor.mizyk@rib-software.com>
2026-02-18 20:07:41 +0100

    added file storage service

--- FileManagement/FileProcessing/Application/Interfaces/FileStorage/IFileStorage.cs
+++ FileManagement/FileProcessing/Application/Interfaces/FileStorage/IFileStorage.cs
@@ -5,5 +5,6 @@
         Task UploadAsync(string path, Stream content, string containerName, string? contentType, CancellationToken cancellationToken);
         Task DeleteAsync(string path, string containerName, CancellationToken cancellationToken);
         Task<string> GetSasUrl(string blobPath, string containerName, CancellationToken cancellationToken);
+        Task<Stream?> GetFileStream(string blobPath, string containerName, CancellationToken cancellationToken = default);
     }
 }
--- FileManagement/FileProcessing/Infrastructure/FileStorage/FileStorage.cs
+++ FileManagement/FileProcessing/Infrastructure/FileStorage/FileStorage.cs
@@ -5,11 +5,12 @@ using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Azure.Storage.Sas;
 
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Infrastructure.FileStorage
 {
-    internal class AzureFileStorage(IOptions<FileStorageConfiguration> optionsDI) : IFileStorage
+    internal class AzureFileStorage(IOptions<FileStorageConfiguration> optionsDI, ILogger<AzureFileStorage> logger) : IFileStorage
     {
         private FileStorageConfiguration config = optionsDI.Value;
         public async Task DeleteAsync(string path, string containerName, CancellationToken cancellationToken)
@@ -20,12 +21,17 @@ namespace Infrastructure.FileStorage
             await containerClient.DeleteBlobIfExistsAsync(path, cancellationToken: cancellationToken);
         }
 
+        private void ThrowExceptionIfNoConnectionStringToBlob()
+        {
+            if (string.IsNullOrEmpty(config.ConnectionName)) throw new Exception("Blob storage connection string is not defined");
+        }
+
         public async Task UploadAsync(string path, Stream content,
             string containerName,
             string contentType,
             CancellationToken cancellationToken = default)
         {
-            if (string.IsNullOrEmpty(config.ConnectionName)) throw new Exception("Blob storage connection string is not defined");
+            ThrowExceptionIfNoConnectionStringToBlob();
 
             BlobContainerClient containerClient = new(config.ConnectionName, containerName);
 
@@ -44,14 +50,35 @@ namespace Infrastructure.FileStorage
 
         public async Task<string> GetSasUrl(string blobPath, string containerName, CancellationToken cancellationToken = default)
         {
-            if (string.IsNullOrEmpty(config.ConnectionName)) throw new Exception("Blob storage connection string is not defined");
+            ThrowExceptionIfNoConnectionStringToBlob();
 
             BlobContainerClient containerClient = new(config.ConnectionName, containerName);
             BlobClient blobClient = containerClient.GetBlobClient(blobPath);
+
             BlobSasBuilder blobSasBuilder = new(BlobSasPermissions.All, DateTimeOffset.Now.AddMinutes(10));
 
             Uri sasUrl = blobClient.GenerateSasUri(blobSasBuilder);
             return sasUrl.AbsoluteUri;
+        }
+
+        public async Task<Stream?> GetFileStream(string blobPath, string containerName, CancellationToken cancellationToken = default)
+        {
+            ThrowExceptionIfNoConnectionStringToBlob();
+
+            BlobContainerClient containerClient = new(config.ConnectionName, containerName);
+            BlobClient blobClient = containerClient.GetBlobClient(blobPath);
+
+            var fileStream = await blobClient.DownloadStreamingAsync(cancellationToken: cancellationToken);
+
+            try
+            {
+                return fileStream.Value.Content;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"GetFileStream failed, blobPath: {blobPath}, containerName: {containerName}");
+                return null;
+            }
         }
     }
 }
